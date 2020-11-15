8545d02c47daec1e05c874f0e63db22da506d743
Тарас Крупко <121-19-6-krupko@knute.edu.ua>
2020-11-15 20:38:16 +0200

    Есть возможность выбора файла из всплывающего окна

--- OOP_LB6_2/MainWindow.xaml.cs
+++ OOP_LB6_2/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using OOP_LB6_2.DialogBoxes;
+﻿using OOP_LB6_2.Classes;
+using OOP_LB6_2.DialogBoxes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,15 +27,32 @@ namespace OOP_LB6_2
             InitializeComponent();
         }
 
+        public PlayerItem Player { get; set; }
+
         private void MenuItem_Open_OnClick(object sender, RoutedEventArgs e)
         {
-            FileSelectionWindow fileSelection = new FileSelectionWindow() { Owner = this };
+            FileSelectionWindow fileSelection = new FileSelectionWindow(this) { Owner = this };
             fileSelection.Show();
         }
 
         private void MenuItem_Close_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            testtext.Text = Player.Url + Player.Name + Player.Type;
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Record_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
